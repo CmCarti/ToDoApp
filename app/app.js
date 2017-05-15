ae82abef197053cb1932e7c537368390e47e1835
Chad Cartier <cptkrush@Chads-MacBook-Pro.local>
2017-05-15 13:52:54 -0400

    App is actually starting to come together

--- app/app.js
+++ app/app.js
@@ -16,16 +16,30 @@ function addTodo(todoObject) {
   renderTodos(toDoList);
 }
 function removeTodo(todo) {
-  // 1. pop todo
+  // 1. slice todo
+  toDoList.splice(todo,1);
+  renderTodos(toDoList);
   // 2. render todos
 }
-
+function editTodo(todo, barClass){
+  const editBar = document.querySelector(`#${barClass}`);
+  toDoList[todo] = editBar.value;
+  renderTodos(toDoList);
+}
 function renderTodos(toDoList) {
    // 1. render the todos to the screen
    const canvas = document.querySelector('.todo__canvas--js');
    canvas.innerHTML = '';
-   toDoList.forEach(item => {
-     canvas.innerHTML += `<div class="todo__item--js">${item}</div>`;
+   toDoList.forEach((item, index) => {
+     canvas.innerHTML += `<div class="todo__item--js">
+     <h3 class="todo__name">${item}</h3>
+     <i class="todo__remove--js fa fa-times" onclick="removeTodo(${index})"></i>
+     <i class="todo__complete--js fa fa-check"></i>
+     <i class="todo__edit--js fa fa-pencil"></i>
+     <form onsubmit="editTodo(${index},'todo__edit-${index}')">
+       <input id="todo__edit-${index}">
+     </form>
+   </div>`;
    });
    // 2. Give the todos event listeners
 }
